30ed48466c82a8ac7fe6f86c46462b2fa4c9102e
balav08 <54432605+balav08@users.noreply.github.com>
2026-09-08 17:35:41 +0530

    RDKEMW-21989 : Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -50,7 +50,7 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-usbmassstorage \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/secure_wrappermock.h \
 -Wall -Werror -Wno-error=format \
 -Wl,-wrap,system -Wl,-wrap,popen -Wl,-wrap,syslog \
--DDISABLE_SECURITY_TOKEN -DUSE_THUNDER_R4=ON -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
+-DDISABLE_SECURITY_TOKEN -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
 
 
 cmake --build build/entservices-usbmassstorage --target install
